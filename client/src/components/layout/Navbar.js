03aebd6be5fa324e29ae2831c7f61dca725bdf76
Dwivedi Vishal Sridhar Arya <vidyutron@live.com>
2018-09-07 02:42:54 +0530

    Addition: 1. Private Router-React 2. UI Elmenent Component creation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileAction";
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -89,5 +91,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
