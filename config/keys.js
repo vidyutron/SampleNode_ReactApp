7e3e7e7d8d3d9091cd17460d1204e9806d614908
Dwivedi Vishal Sridhar Arya <vidyutron@live.com>
2018-09-19 23:19:41 +0530

    Keys config changes

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongouri:
-    "mongodb://vidyutron:FoisbycsunRoas5@ds125342.mlab.com:25342/socialnetworkdev",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
